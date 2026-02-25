5ee528d1e5577f07328ccc128d7f1d94a5091b69
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2026-02-25 14:15:11 +0100

    Followed toturial to make a notes app using WinUI

---
+++ WinUINotes/WinUINotes/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUINotes
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // ↓ Add this. ↓
+            // Hide the default system title bar.
+            ExtendsContentIntoTitleBar = true;
+            // Replace system title bar with the WinUI TitleBar.
+            SetTitleBar(AppTitleBar);
+            // ↑ Add this. ↑
+        }
+
+        private void AppTitleBar_BackRequested(TitleBar sender, object args)
+        {
+            // ↓ Add this. ↓
+            if (rootFrame.CanGoBack == true)
+            {
+                rootFrame.GoBack();
+            }
+            // ↑ Add this. ↑
+        }
+    }
+}
